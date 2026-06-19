986fdfdad9ec7d67b4c23d2a1c28f1230d16fa75
boban16 <olivieri88@gmail.com>
2026-06-19 14:48:15 +0200

    feat: enable groups routes at /v1/groups

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ fastify.register(ArchiveRoutes, { prefix: '/v1/archive' })
 fastify.register(CategoryExclusionRoutes, { prefix: '/v1/category-exclusion' })
 fastify.register(VehiclesRoutes, { prefix: '/v1/vehicles' })
 fastify.register(CashFlowRoutes, { prefix: '/v1/cash-flow' })
-// fastify.register(GroupsRoutes, { prefix: '/v1/groups' }) // Disabled - using consultative approach in report.js
+fastify.register(GroupsRoutes, { prefix: '/v1/groups' })
 // fastify.register(SetupRoutes, { prefix: '/v1/setup' })
 // fastify.register(MLAnalysisRoutes, { prefix: '/v1/ml-analysis' })
 
